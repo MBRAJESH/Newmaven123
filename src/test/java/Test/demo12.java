ae11fe84c879420556f23a32abe18e5c9de68379
Pluto <Pluto@DESKTOP-JVK1AED>
2019-12-14 12:35:41 +0530

    second commit

---
+++ src/test/java/Test/demo12.java
@@ -0,0 +1,18 @@
+package Test;
+
+import org.checkerframework.common.util.report.qual.ReportCall;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demo12
+
+{
+	@Test
+	public void validlogin()
+	{
+		Reporter.log("login to app....", true);
+		
+		
+	}
+
+}
